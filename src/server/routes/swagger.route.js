8343addb11554b277f1b6d8d11c60a4cbfadfde1
codingxxxxx <hc2.minea@gmail.com>
2024-02-10 20:04:45 +0700

    chore: adjust function that initailize swagger

--- src/server/routes/swagger.route.js
+++ src/server/routes/swagger.route.js
@@ -1,17 +1,32 @@
 const router = require('express').Router();
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 const $RefParser = require('json-schema-ref-parser');
 
-const swaggerPath = '../../../swagger.yaml';
-const swaggerDocument = YAML.load(path.resolve(__dirname, swaggerPath));
+const swaggerPath = 'swagger.yaml'
+const apiPath = 'swagger/api.yaml';
+const { paths } =YAML.load(apiPath);
 
-$RefParser.dereference(swaggerDocument)
-    .then(swaggerDocumentResolved => {
-        router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentResolved, {
+Promise.all(
+    paths.map(path => $RefParser.dereference(path))
+)
+.then(docs => {
+  $RefParser
+    .dereference(swaggerPath)
+    .then(swaggerConfig => {
+        // join api paths
+        const allApiPaths = docs.reduce((acu, val) => {
+            if (!acu) acu = {};
+            acu = { ...acu, ...val };
+            return acu;
+        })
+
+        swaggerConfig.paths = allApiPaths;
+
+        router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig, {
             explorer: true
-        }))
+        }));
     })
+})
 
 module.exports = router;
